25efd4a1aa0672cad1b7f3d5fb0ae5cddca32f03
Joaquin Anun <joacoanun@gmail.com>
2026-05-12 18:46:54 -0300

    fix: calculation was wrong change from year model to model

--- electric_vehicles/models/marts/top_10_electrical_vehicles_registration_count.sql
+++ electric_vehicles/models/marts/top_10_electrical_vehicles_registration_count.sql
@@ -1,9 +1,9 @@
 {{ config(materialized='view')}}
 -- How many electric vehicles are registered per year
 SELECT 
-	model_year,
+	model,
 	COUNT(*) AS vechicles_count
 FROM {{ ref('electrical_vehicles_silver')}}
-GROUP BY model_year
+GROUP BY model
 ORDER BY vechicles_count DESC
 LIMIT 10
