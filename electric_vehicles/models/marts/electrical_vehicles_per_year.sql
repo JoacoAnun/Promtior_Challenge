9d55a05e34d414b8ecbbbff4e54ce1bcf2653f49
Joaquin Anun <joacoanun@gmail.com>
2026-05-12 11:27:21 -0300

    fix: remove silver reference

--- electric_vehicles/models/marts/electrical_vehicles_per_year.sql
+++ electric_vehicles/models/marts/electrical_vehicles_per_year.sql
@@ -2,7 +2,7 @@
 -- How many electric vehicles are registered per year
 SELECT 
 	model_year,
-	COUNT(*) AS vechicles_count
-FROM {{ source('silver', 'electrical_vehicles_silver')}}
+	COUNT(*)
+FROM {{ ref('electrical_vehicles_silver')}}
 GROUP BY model_year
 ORDER BY model_year
