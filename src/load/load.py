9aea590f69ad3df20101e118817ef956b1c1ac85
Joaquin Anun <joacoanun@gmail.com>
2026-05-15 08:47:34 -0300

    fix: default to localhost to test outside containers

--- src/load/load.py
+++ src/load/load.py
@@ -8,7 +8,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DB_HOST = "localhost"
+DB_HOST = os.getenv("DB_HOST", "localhost")
 DB_PORT = os.getenv("DB_PORT")
 DB_NAME = os.getenv("POSTGRES_DB")
 DB_USER = os.getenv("POSTGRES_USER")
